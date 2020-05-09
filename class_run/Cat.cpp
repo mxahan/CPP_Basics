71e7601f4c8c8cf2901141345d5bcf28ee7a87aa
mxahan <smxahsan@gmail.com>
2020-05-09 02:13:06 -0400

    new

---
+++ class_run/Cat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "Cat.h"
+
+using namespace std;
+
+
+Cat::Cat() {
+	cout << "Cat created." << endl;
+
+	happy = true;
+}
+
+Cat::~Cat() {
+	cout << "Cat destroyed." << endl;
+}
+
+void Cat::speak() {
+	if (happy){
+		cout<<"nice"<<endl;
+	}else{
+		cout<<"not nice"<<endl;
+	}
+}
---
+++ class_run/Cat.h
@@ -0,0 +1,17 @@
+
+#ifndef CAT_H_
+#define CAT_H_
+
+class Cat {
+private:
+
+
+public:
+	bool happy;
+	void speak();
+	Cat();
+	~Cat();
+};
+
+
+#endif /* CAT_H_ */
